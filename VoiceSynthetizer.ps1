1eca1fd5d0d2a13083463d618ccc419617b27927
St0n14 <44037022+St0n14@users.noreply.github.com>
2022-09-06 20:52:58 +0200

    Add files via upload

---
+++ VoiceSynthetizer.ps1
@@ -0,0 +1,9 @@
+﻿[Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null
+#notepad.exe 
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/St0n14/Pranks/main/PrankPhrases.txt" -OutFile $HOME\prankphrases.txt 
+$phrases = Get-Content $HOME\prankphrases.txt 
+$phrase = Get-Random $phrases 
+#$phrase = "You get pwned"
+$object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+$object.SelectVoiceByHints('Male')
+$object.Speak($phrase)
